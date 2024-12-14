82d8b6ab93304c0e73002033ca95441fe27adaf4
Enrique Henderson <ehenderson@surgeforward.com>
2024-12-13 20:33:21 -0700

    Initial Commit

---
+++ SimpleBudgetApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SimpleBudgetApp
+//
+//  Created by Enrique Henderson on 12/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SimpleBudgetApp/SimpleBudgetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleBudgetAppApp.swift
+//  SimpleBudgetApp
+//
+//  Created by Enrique Henderson on 12/13/24.
+//
+
+import SwiftUI
+
+@main
+struct SimpleBudgetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
